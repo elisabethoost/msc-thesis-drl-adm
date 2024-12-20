e128ba6f916042e4b703a45cf8cad81e1cd3508c
Pieter Becking <ph.becking@gmail.com>
2024-12-20 21:23:16 +0100

    refactor print_state_nicely function

--- main.py
+++ main.py
@@ -367,7 +367,7 @@ if __name__ == "__main__":
     early_stopping_flag = False
     CROSS_VAL_INTERVAL = 2
     printing_intermediate_results = False
-    save_folder = "101-novel-run"
+    save_folder = "102-novel-run"
     TESTING_FOLDERS_PATH = "data/Testing/6ac-7-diverse/"
 
     if not os.path.exists(save_folder):
@@ -377,7 +377,8 @@ if __name__ == "__main__":
         # "data/Training/6ac-100-stochastic-low/",
         # "data/Training/6ac-100-stochastic-medium/",
         # "data/Training/6ac-100-stochastic-high/",
-        "data/Training/6ac-7-diverse/",
+        # "data/Training/6ac-7-diverse/",
+        "data/Training/3ac-5-deterministic-na/",
     ]
 
     if args.seed is None and args.training_folder is None:
