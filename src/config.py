fbaf387bae0cbb9391fbccf68acbcbcc431d8c5b
Pieter Becking <ph.becking@gmail.com>
2025-01-09 20:01:42 +0100

    correct config values restored

--- src/config.py
+++ src/config.py
@@ -19,18 +19,20 @@ TIMESTEP_HOURS = 1  # Length of each timestep in hours
 
 
 DUMMY_VALUE = -999  # Dummy value for padding
+
+
 # Reward and Penalty Values
-RESOLVED_CONFLICT_REWARD = 50000     # Reward for resolving a conflict
+RESOLVED_CONFLICT_REWARD = 30000     # Reward for resolving a conflict
 DELAY_MINUTE_PENALTY = 50           # Penalty per minute of delay
-MAX_DELAY_PENALTY = np.inf            # Maximum penalty for delay
-NO_ACTION_PENALTY = 20000               # Penalty for no action while conflict(s) exist
+MAX_DELAY_PENALTY = 25000000            # Maximum penalty for delay
+NO_ACTION_PENALTY = 0.2               # Penalty for no action while conflict(s) exist
 CANCELLED_FLIGHT_PENALTY = 5000    # Penalty for cancelling a flight
 LAST_MINUTE_THRESHOLD = 120           # Threshold for last-minute changes in minutes
 LAST_MINUTE_FLIGHT_PENALTY = 300      # Penalty for last-minute flight changes  
 AHEAD_BONUS_PER_MINUTE = 0.05                # Reward for proactive flight changes
-TIME_MINUTE_PENALTY = 10                 # penalty for every minute passed, each timestep cumulatively
+TIME_MINUTE_PENALTY = 0.1                 # penalty for every minute passed, each timestep cumulatively
 TERMINATION_REWARD = 0                  # Reward for terminating the episode
-TAIL_SWAP_COST = 1000
+TAIL_SWAP_COST = 100
 
 
 # Environment Settings
